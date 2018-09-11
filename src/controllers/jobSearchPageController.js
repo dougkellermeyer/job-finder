4003458de87463062b698cd1d483db189cb5bf10
Kellermeyer,Doug <dkellermeyer#paychex.com>
2018-09-11 16:04:42 -0400

    added checkbox for job search

--- src/controllers/jobSearchPageController.js
+++ src/controllers/jobSearchPageController.js
@@ -1,4 +1,4 @@
-app.controller("jobSearchPageController", function(){
+app.controller("jobSearchPageController", function($scope){
     console.log("job search ran!")
 
     var vm = this;
@@ -8,4 +8,25 @@ app.controller("jobSearchPageController", function(){
         'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
         'WY')
         .split(' ').map(function (state) { return { abbrev: state }; });
+    
+    $scope.items = ["Coding","Leading teams","HTML","JavaScript","CSS","Databases"];
+    $scope.selected = [];
+
+    function showSelected() {
+        console.log($scope.selected);
+    }
+
+    $scope.toggle = function (item, list) {
+        var userSelected = list.indexOf(item);
+        if (userSelected > -1) {
+            list.splice(userSelected, 1);
+        }
+        else {
+            list.push(item);
+        }
+    };
+
+    $scope.exists = function (item, list) {
+        return list.indexOf(item) > -1;
+    };
 });
