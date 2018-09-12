4795d8dca77d4bc2546d6a3948e2eb1c96c458cc
Kellermeyer,Doug <dkellermeyer#paychex.com>
2018-09-12 15:21:08 -0400

    added headers and sort to job tables

--- src/controllers/jobSearchResults.js
+++ src/controllers/jobSearchResults.js
@@ -1,3 +1,13 @@
-app.controller("jobSearchResultsController", function($scope){
+app.controller("jobSearchResultsController", function($scope, searchSvc){
     console.log("job search results working!")
+
+    var vm = this;
+
+    searchSvc.getJobs().then(function(res){
+        vm.jobs = res.data.jobs;
+        console.log("all jobs received");
+    })
+
+    $scope.orderByField = 'positionName';
+    $scope.reverseSort = false;
 });
