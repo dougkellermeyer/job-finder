588e6e601ebdcdf867344facc22967ee18a7c8be
Kellermeyer,Doug <dkellermeyer#paychex.com>
2018-09-20 12:56:47 -0400

    removed broken transition

--- src/controllers/jobSearchPageController.js
+++ src/controllers/jobSearchPageController.js
@@ -45,5 +45,4 @@ app.controller("jobSearchPageController",['$scope','$state','$transitions', func
 
     //implement a transition so we can introduct a loading screen before going to jobSearchResults 
 
-    $transitions.onStart( {})
 }]);
