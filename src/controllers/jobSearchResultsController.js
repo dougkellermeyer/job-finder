96f49e03a6493ecb0bdd2af0939c83edd54371f6
Kellermeyer,Doug <dkellermeyer#paychex.com>
2018-09-21 12:02:50 -0400

    moved filter service to services

--- src/controllers/jobSearchResultsController.js
+++ src/controllers/jobSearchResultsController.js
@@ -70,24 +70,4 @@ app.controller("jobSearchResultsController",
     };
 }]);
 
-app.filter('keywordFilter', function(){
-    return function(allJobs, checked, $timeout){
-        //need to split checked into an array
-    
-        var output = [];
 
-        if(checked == "" || checked == undefined){
-            return allJobs;
-        }
-        else {
-            var checkedArray = checked.split(",");
-            angular.forEach(allJobs,function(value){                
-                if(checkedArray.includes(value.positionName)){
-                    output.push(value);
-                }
-            })
-            return output;
-        }
-    }
-    
-});
---
+++ src/services/filterService.js
@@ -0,0 +1,21 @@
+app.filter('keywordFilter', function(){
+    return function(allJobs, checked, $timeout){
+        //need to split checked into an array
+    
+        var output = [];
+
+        if(checked == "" || checked == undefined){
+            return allJobs;
+        }
+        else {
+            var checkedArray = checked.split(",");
+            angular.forEach(allJobs,function(value){                
+                if(checkedArray.includes(value.positionName)){
+                    output.push(value);
+                }
+            })
+            return output;
+        }
+    }
+    
+});
