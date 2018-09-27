0b0f809dc1f9129e381ed5955f91c7baac40dfa5
Kellermeyer,Doug <dkellermeyer#paychex.com>
2018-09-27 09:53:57 -0400

    added savedJob details area and loader

--- src/components/sideNav/sideNavController.js
+++ src/components/sideNav/sideNavController.js
@@ -1,5 +1,6 @@
 app.controller("SideNavController", function(){
-
+//inject the saveJobSvc so that we can display the jobs in the dropdown
+//as they populate on the savedJobs.html page
 })
  
  app.component('jobSidenav',{
---
+++ src/controllers/savedJobDetailsController.js
@@ -0,0 +1,28 @@
+app.config(function($stateProvider){
+    $stateProvider
+        .state('savedJobs.details', {
+            url: '/details',
+            templateUrl: 'savedJobsDetails.html',
+            controller: "savedJobsDetailsController",
+            controllerAs: "$ctrl"
+        })
+});
+
+app.controller("savedJobsDetailsController",
+function($scope,$timeout, saveJobSvc){
+
+this.saveJobSvc = saveJobSvc;
+
+vm = this;
+
+this.$onInit = function(){
+    $timeout(function(){}, 1500)
+            .then(function(){
+                vm.loading = false;
+            })
+}
+
+
+vm.loading = true;
+
+});
