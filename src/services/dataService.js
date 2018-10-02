aaf2aa4474fcce80050ba79ed1710509e349e184
Kellermeyer,Doug <dkellermeyer#paychex.com>
2018-10-02 10:55:59 -0400

    hard coded id

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,14 +1,12 @@
 //http service to job API
 app.service('searchSvc', function($http, url){
 
-    var jobDetailsArray = [];
-
     this.getJobs = function(){
         return $http.get(url)    
     };
     this.getOneJob = (job) => {
-        console.log(job._id);
-        console.log(typeof job._id);
-        return $http.get(url + job._id)
+        // console.log(job._id);
+        // console.log(typeof job._id);
+        return $http.get(url + "5ba8de0aa30f4a656885a727")
     };
 });
