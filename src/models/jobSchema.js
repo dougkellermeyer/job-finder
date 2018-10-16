8c4e569afa94eda53f4664372abc6dfb4dd4607d
Kellermeyer,Doug <dkellermeyer#paychex.com>
2018-10-16 11:54:54 -0400

    added CRUD functions to API

--- src/models/jobSchema.js
+++ src/models/jobSchema.js
@@ -3,15 +3,15 @@ var mongoose = require('mongoose');
 var jobSchema = mongoose.Schema({
     company: {
         type: String,
-        required: true
+        // required: true
     },
     positionName: {
         type: String,
-        required: true
+        // required: true
     },
     positionDescription: {
         type: String,
-        required: true
+        // required: true
     },
     location: {
         state: {
@@ -32,14 +32,33 @@ var jobSchema = mongoose.Schema({
 
 var Job = module.exports = mongoose.model('Job', jobSchema, 'jobs');
 
-//get all jobs
-
+//Will GET  all jobs in DB
 module.exports.getNewJobs = (callback) => {
     Job.find(callback);
 };
 
-//get one job
+//Will GET job with specified id
 module.exports.getJobById = (id, callback) => {
     Job.findById(id,callback);
 };
 
+//Will CREATE a new job in DB
+module.exports.createJob = (job, callback) => {
+    Job.create(job,callback);
+};
+
+//Will DELETE a job with specified id
+module.exports.removeJob = (id, callback) => {
+    var query = {_id: id};
+    Job.remove(query,callback);
+};
+
+//Will UPDATE a job wiht specified id
+module.exports.updateJob = (id, job, options, callback) => {
+    var query = {_id: id};
+    var update = {
+        company: job.company,
+        positionName: job.positionName,
+    }
+    Job.findOneAndUpdate(query, update, options, callback);
+};
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose').set('debug', true);
 var router = express();
 
+router.use(bodyParser.json());
+
 Job = require('./models/jobSchema');
 Test = require('./models/test');
 
@@ -13,27 +15,65 @@ var PORT = 8080;
 mongoose.connect('mongodb://localhost/jobs');
 var db = mongoose.connection;
 
+//message for hitting the API
 router.get('/', (req, res) => {
     res.send('Please use the /api/jobs');
 });
 
+//Will GET  all jobs in DB
 router.get('/api/jobs', (req, res) => {
-    Job.getNewJobs((err,jobs)=>{
-        if(err){
+    Job.getNewJobs((err, jobs) => {
+        if (err) {
             throw err;
         }
         res.json(jobs);
     })
 });
 
-router.get('/api/jobs/:_id', (req, res) => {
-    Job.getJobById(req.params._id,(err,job)=>{
-        if(err){
+//Will CREATE a new job in DB
+router.post('/api/jobs/', (req, res) => {
+    Job.createJob(req.body, (err, job) => {
+        if (err) {
             throw err;
         }
         res.json(job);
     });
 });
+
+//Will GET job with specified id
+router.get('/api/jobs/:_id', (req, res) => {
+    Job.getJobById(req.params._id, (err, job) => {
+        if (err) {
+            throw err;
+        }
+        res.json(job);
+    });
+});
+
+//Will UPDATE job with specified id
+router.put('/api/jobs/:_id', (req, res) => {
+    var id = req.params._id;
+    var job = req.body;
+    Job.updateJob(id, job, {}, (err, job) => {
+        if (err) {
+            throw err;
+        }
+        res.json(job);
+    });
+});
+
+//Will DELETE job with specified id
+router.delete('/api/jobs/:_id', (req, res) => {
+    var id = req.params._id;
+    Job.removeJob(id, (err, job) => {
+        if (err) {
+            throw err;
+        }
+        res.json(job);
+    });
+});
+
+
 
 router.listen(PORT);
 console.log("running app on port " + PORT + " !")
