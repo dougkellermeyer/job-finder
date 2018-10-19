6f9916afe2aced8e09c8dfb194b1d45cf6a154f9
Kellermeyer,Doug <dkellermeyer#paychex.com>
2018-10-19 10:37:08 -0400

    package updates:

--- src/controllers/landingPageController.js
+++ src/controllers/landingPageController.js
@@ -1,4 +1,4 @@
-app.config(function($stateProvider, $urlRouterProvider, $locationProvider ){
+app.config(function($stateProvider, $urlRouterProvider){
     $stateProvider
         .state('landingPage', {
             url: '/',
@@ -7,7 +7,6 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider ){
             controllerAs: '$ctrl'
         })
     $urlRouterProvider.otherwise('/');
-    $locationProvider.html5Mode(true);
 });
 
 app.controller('landingPageController', function(){
