b5594aece73d6521c041a1ab1340ca626a25dd79
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2019-09-21 16:17:48 -0400

    rebuilt package-lock.json

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose').set('debug', true);
-var env = require('dotenv');
+// var env = require('dotenv');
 
 var config = require('./src/api/config')
 var routes = require('./src/api/routes')
@@ -17,6 +17,6 @@ if (config.seedDB){
     require('./src/api/seeddb');
 }
 
-app.listen(config.server.port, ()=>{
+app.listen(config.server.port, () => {
     console.log('running app on port ' + config.server.port + '!')
 });
